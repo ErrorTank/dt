75b0f07be33d594e62a8d61aad5aec31d4ad2990
Tuan Anh <ncq998@gmail.com>
2019-07-17 09:52:45 +0700

    dasdasdas

--- visits/index.js
+++ visits/index.js
@@ -5,10 +5,16 @@ const app = express();
 
 const client = redis.createClient();
 
+client.set("visits", 0);
+
 app.get("/", (req, res) => {
-  res.send("Hi there! ok");
+  client.get("visits", (err, visits) => {
+    res.send("Number of visits is: ", visits);
+    client.set("visits", parseInt(visits) + 1);
+  });
+
 });
 
-app.listen(8080, () => {
-  console.log("Listening on port 4000!")
+app.listen(3000, () => {
+  console.log("Listening on port 3000!")
 });
